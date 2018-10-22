7a79a42a7fbe35a659f662c76a6737fff9c3fe10
AbdulrahmanQassim <32072473+AbdulrahmanQassim@users.noreply.github.com>
2018-10-23 01:28:04 +0300

    Add files via upload

---
+++ Q3.dart
@@ -0,0 +1,33 @@
+//
+//   //Q4: Write a function that combines two lists by alternatingly taking elements,
+//   //first
+//void main() {
+//  var num = [1, 2, 3];   //list 1
+//  var let = ['a', 'b', 'c']; // list 2
+//  for (var i = 0; i < num.length; i++) {   //for loop
+//    var results = '${num[i]} ${let[i]}'; //merge var
+//    print(results);
+//  }
+//}
+//print('..................................');
+//    //second
+//void main() {
+//  var numbers = [1, 2, 5, 8, 0]; //list 1
+//  var letters = [9, 4, 8, 7, 6]; //list 2
+//  for (var j = 0; j< numbers.length; j++) {      //for loop
+//    var result = '${numbers[j]} ${letters[j]}'; //merge var
+//    print(result);
+//
+//  }
+//}
+//print('..................................');
+
+
+
+
+
+
+
+
+
+
---
+++ main.dart
@@ -0,0 +1,186 @@
+//Q2 Write a program that accepts an array of numbers and returns an array of numbers in written form e.g.
+//[1,4,6]
+//[0,0,6,2,7]
+//
+//[5,4,3,2,1,5,8]
+
+//sol for first list
+void main() {
+  var list1 = [1, 4, 6];                   //define variables
+  for (var x = 0; x < list1.length; x++) {
+    switch (list1[x]) {
+      case 1:
+        {
+          print("for first list ${list1[x]} one ");
+        }
+        break;
+
+      case 2:
+        {
+          print("for first list ${list1[x]} two");
+        }
+        break;
+
+      case 3:
+        {
+          print("for first list ${list1[x]} three");
+        }
+        break;
+      case 4:
+        {
+          print("for first list ${list1[x]} four");
+        }
+        break;
+      case 5:
+        {
+          print("for first list ${list1[x]} five");
+        }
+        break;
+      case 6:
+        {
+          print("for first list ${list1[x]} six");
+        }
+        break;
+      case 7:
+        {
+          print("for first list ${list1[x]} seven");
+        }
+        break;
+      case 8:
+        {
+          print("for first list ${list1[x]} eight");
+        }
+        break;
+      case 9:
+        {
+          print("for first list ${list1[x]} nine");
+        }
+        break;
+      case 0:
+        {
+          print("for first list ${list1[x]} zero");
+        }
+        break;
+    }
+  }
+  print('..................................');
+  //sol for second list
+
+  var list2 = [0,0,6,2,7];//define variables
+  for (var x = 0; x < list2.length; x++) {
+    switch (list2[x]) {
+      case 1:
+        {
+          print("for second list ${list2[x]} one ");
+        }
+        break;
+
+      case 2:
+        {
+          print("for second list ${list2[x]} two");
+        }
+        break;
+
+      case 3:
+        {
+          print("for second list ${list2[x]} three");
+        }
+        break;
+      case 4:
+        {
+          print("for second list ${list2[x]} four");
+        }
+        break;
+      case 5:
+        {
+          print("for second list ${list2[x]} five");
+        }
+        break;
+      case 6:
+        {
+          print("for second list ${list2[x]} six");
+        }
+        break;
+      case 7:
+        {
+          print("for second list ${list2[x]} seven");
+        }
+        break;
+      case 8:
+        {
+          print("for second listt ${list2[x]} eight");
+        }
+        break;
+      case 9:
+        {
+          print("for second list ${list2[x]} nine");
+        }
+        break;
+      case 0:
+        {
+          print("for second list ${list2[x]} zero");
+        }
+        break;
+    }
+  }
+  print('..................................');
+
+  //third list
+  var list3 = [5,4,3,2,1,5,8]; //define var
+  for (var x = 0; x < list3.length; x++) {
+    switch (list3[x]) {
+      case 1:
+        {
+          print("for third list ${list3[x]} one ");
+        }
+        break;
+
+      case 2:
+        {
+          print("for third list ${list3[x]} two");
+        }
+        break;
+
+      case 3:
+        {
+          print("for third list ${list3[x]} three");
+        }
+        break;
+      case 4:
+        {
+          print("for third list ${list3[x]} four");
+        }
+        break;
+      case 5:
+        {
+          print("for third list ${list3[x]} five");
+        }
+        break;
+      case 6:
+        {
+          print("for third list ${list3[x]} six");
+        }
+        break;
+      case 7:
+        {
+          print("for third list ${list3[x]} seven");
+        }
+        break;
+      case 8:
+        {
+          print("for third listt ${list3[x]} eight");
+        }
+        break;
+      case 9:
+        {
+          print("for third list ${list3[x]} nine");
+        }
+        break;
+      case 0:
+        {
+          print("for third list ${list3[x]} zero");
+        }
+        break;
+    }
+  }
+}
